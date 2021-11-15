709f6aa24ee285eae238d1f650baf9cfb7281e60
tangrisheng <icerain1111@163.com>
2021-11-15 17:38:19 +0800

    update java demo

--- java/Readme.md
+++ java/Readme.md
@@ -1,7 +1,7 @@
-java demo
+java 1.8 demo
 ===========
 # 如何开始
-- 建议使用 Java 16 运行
+- 建议使用 Java 1.8 运行
 - 替换 Example.java  中的公钥和私钥
 - 运行
 ```
--- java/src/Example.java
+++ java/src/Example.java
@@ -1,15 +1,14 @@
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.Objects;
 
 public class Example {
 
     public static void main(String[] args) throws Exception {
-        String publicKey = "vip9";
-        String secretKey = "tpvip9";
+        String publicKey = "XXX";
+        String secretKey = "XXX";
 
         SeniverseUtils seniverseUtils = new SeniverseUtils();
 
@@ -18,15 +17,30 @@ public class Example {
         seniverseUtils.addParameter("public_key", publicKey);
         seniverseUtils.addParameter("locations", "29.5617:120.0962");
 
-        String urlStr = seniverseUtils.getUrl("https://api.seniverse.com/v4", secretKey);
+        try {
+            String urlStr = seniverseUtils.getUrl("https://api.seniverse.com/v4", secretKey);
 
-        URL url = new URL(urlStr);
-        URLConnection conn = url.openConnection();
+            URL url = new URL(urlStr);
 
-        InputStream stream = conn.getInputStream();
+            URLConnection conn = url.openConnection();
 
-        System.out.println("");
-        System.out.println("Url: ".concat(urlStr));
-        System.out.println(new String(stream.readAllBytes(), StandardCharsets.UTF_8));
+            InputStream stream = conn.getInputStream();
+            ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = stream.read(buffer)) != -1) {
+                outSteam.write(buffer, 0, len);
+            }
+            outSteam.close();
+            stream.close();
+            String result = outSteam.toString();
+
+            System.out.println("");
+            System.out.println("url: ".concat(urlStr));
+            System.out.println(result);
+        } catch (Error error) {
+            System.err.println(error.getMessage());
+        }
     }
 }
